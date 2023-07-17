91ee0db26f5ae80ed2e638908de724733bb5b9e4
sareenami17 <sareenami17gmail.com>
2023-07-17 11:01:28 +0530

    added

---
+++ ecommerce/eshop/eshopapp/views.py
@@ -0,0 +1,41 @@
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from eshopapp.models import Category, Products
+from django.core.paginator import Paginator, EmptyPage, InvalidPage
+
+
+# Create your views here.
+def allProdCat(request, c_slug=None):
+    c_page = None
+    products = None
+    if c_slug != None:
+        c_page = get_object_or_404(Category, slug=c_slug)
+        products = Products.objects.all().filter(category=c_page, available=True)
+    else:
+        products = Products.objects.all().filter(available=True)
+
+    paginator = Paginator(products,2 )  # Change the number 5 to your desired number of products per page
+
+    try:
+        page = int(request.GET.get('page', '1'))
+    except:
+        page = 1
+
+    try:
+        products = paginator.page(page)
+    except (EmptyPage, InvalidPage):
+        products = paginator.page(paginator.num_pages)
+
+    # Rest of your code...
+
+
+
+    return render(request, "category.html", {'category': c_page, 'products': products})
+
+
+def proDetail(request, c_slug, product_slug):
+    try:
+        products = Products.objects.get(category__slug=c_slug, slug=product_slug)
+    except Exception as e:
+        raise e
+    return render(request, "product.html", {'products': products})
